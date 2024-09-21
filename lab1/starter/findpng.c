4e9bcb0e37e8171261f55f8b5e47d942e5cc3eb3
Ha-Young Son <h24son@uwaterloo.ca>
2024-09-21 18:17:44 -0400

    modified findpng - works as expected

--- lab1/starter/findpng.c
+++ lab1/starter/findpng.c
@@ -3,81 +3,93 @@
 #include <string.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include "lab_png.h"
 
+#define PATH_MAX 4096
+
 typedef unsigned char U8;
-typedef unsigned int U32;
 
-void check_file(const char *filepath)
-{
-    FILE *fp = fopen(filepath, "rb");
-    if (!fp)
-        return;
-
-    U8 png_header[PNG_SIG_SIZE];
-
-    if (fread(png_header, 1, PNG_SIG_SIZE, fp) == PNG_SIG_SIZE)
-    {
-        if (is_png(png_header, PNG_SIG_SIZE))
-        {
-            printf("%s\n", filepath);
-        }
+/* traverses directory and searches for PNG files */
+void traverse_directory(const char *directory, int *png_found) {
+    
+    /* open directory */
+    DIR *dir = opendir(directory);
+    if (dir == NULL) {
+        perror("opendir");
+        exit(EXIT_FAILURE);
     }
 
-    fclose(fp);
-}
-
-void traverse_directory(const char *directory)
-{
-    DIR *dir = opendir(directory);
     struct dirent *entry;
     struct stat buf;
-    char path[4096];
-    int png_found = 0;
+    char path[PATH_MAX];
 
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-        {
-            continue;
-        }
+    /* read each entry in the directory */
+    while ((entry = readdir(dir)) != NULL) {
 
-        snprintf(path, sizeof(path), "%s/%s", directory, entry->d_name);
+        /* skip current and parent directories */
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            /* create full path of the entry */
+            strcpy(path, directory);
+            strcat(path, "/");
+            strcat(path, entry->d_name);
 
-        if (stat(path, &buf) == -1)
-        {
-            continue;
-        }
+            /* get status of the entry */
+            if (stat(path, &buf) == -1) {
+                perror("stat");
+                continue;
+            }
 
-        if (S_ISREG(buf.st_mode))
-        {
-            check_file(path);
-            png_found = 1;
-        }
-        else if (S_ISDIR(buf.st_mode))
-        {
-            traverse_directory(path);
+            /* if the entry is a directory, traverse it recursively */
+            if (S_ISDIR(buf.st_mode)) {
+                traverse_directory(path, png_found);
+            } else {
+                /* open file and read in binary mode */
+                FILE *fp = fopen(path, "rb");
+
+                if (fp == NULL) {
+                    perror("fopen");
+                    continue;
+                }
+
+                U8 header[PNG_SIG_SIZE];
+
+                /* read first 8 bytes of the file */
+                if (fread(header, 1, PNG_SIG_SIZE, fp) == PNG_SIG_SIZE) {
+                    /* check if it is a PNG */
+                    if (is_png(header, PNG_SIG_SIZE)) {
+                        /* print path of PNG file */
+                        printf("%s\n", path);
+                       *png_found = 1;
+                    }
+                }
+
+                /* close file */
+                fclose(fp);
+            }
         }
     }
 
+    /* close directory */
     closedir(dir);
-
-    if (!png_found)
-    {
-        printf("findpng: No PNG file found\n");
-    }
 }
 
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+
+    /* check for correct number of arguments */
+    if (argc != 2) {
         fprintf(stderr, "Usage: %s <directory>\n", argv[0]);
         return 1;
     }
 
-    traverse_directory(argv[1]);
+    int png_found = 0;
+    /* traverse directory */
+    traverse_directory(argv[1], &png_found);
+
+    /* if no PNG files in directory, print message */
+    if (png_found == 0) {
+        printf("No PNG files found\n");
+    }
+
     return 0;
 }
