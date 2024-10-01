8e0ec71b3e7f7dc814e6e4c3939f1d8654bcb4d4
Lucy Sun <g33sun@uwaterloo.ca>
2024-09-30 23:23:21 -0400

    add catpng.h & cURL/main_write_header_cb.h

---
+++ lab2/cURL/main_write_header_cb.h
@@ -0,0 +1,57 @@
+/*
+ * The code is derived from cURL example and paster.c base code.
+ * The cURL example is at URL:
+ * https://curl.haxx.se/libcurl/c/getinmemory.html
+ * Copyright (C) 1998 - 2018, Daniel Stenberg, <daniel@haxx.se>, et al..
+ *
+ * The paster.c code is
+ * Copyright 2013 Patrick Lam, <p23lam@uwaterloo.ca>.
+ *
+ * Modifications to the code are
+ * Copyright 2018-2019, Yiqing Huang, <yqhuang@uwaterloo.ca>.
+ *
+ * This software may be freely redistributed under the terms of the X11 license.
+ */
+
+/**
+ * @file main_wirte_read_cb.c
+ * @brief cURL write call back to save received data in a user defined memory first
+ *        and then write the data to a file for verification purpose.
+ *        cURL header call back extracts data sequence number from header.
+ * @see https://curl.haxx.se/libcurl/c/getinmemory.html
+ * @see https://curl.haxx.se/libcurl/using/
+ * @see https://ec.haxx.se/callback-write.html
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <curl/curl.h>
+
+#define IMG_URL "http://ece252-1.uwaterloo.ca:2520/image?img=1"
+#define DUM_URL "https://example.com/"
+#define ECE252_HEADER "X-Ece252-Fragment: "
+#define BUF_SIZE 1048576 /* 1024*1024 = 1M */
+#define BUF_INC 524288   /* 1024*512  = 0.5M */
+
+#define max(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+typedef struct recv_buf2
+{
+    char *buf;       /* memory to hold a copy of received data */
+    size_t size;     /* size of valid data in buf in bytes*/
+    size_t max_size; /* max capacity of buf in bytes*/
+    int seq;         /* >=0 sequence number extracted from http header */
+                     /* <0 indicates an invalid seq number */
+} RECV_BUF;
+
+size_t header_cb_curl(char *p_recv, size_t size, size_t nmemb, void *userdata);
+size_t write_cb_curl3(char *p_recv, size_t size, size_t nmemb, void *p_userdata);
+int recv_buf_init(RECV_BUF *ptr, size_t max_size);
+int recv_buf_cleanup(RECV_BUF *ptr);
+int write_file(const char *path, const void *in, size_t len);
---
+++ lab2/catpng.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <curl/curl.h>
+#include <pthread.h>
+#include <arpa/inet.h> /* add for htonl() */
+#include "lab_png.h"
+#include "crc.h"
+#include "zutil.h"
+
+#define NUM_STRIPS 50
+#define MAX_URL_LENGTH 256
+#define DEFAULT_THREAD 1
+#define DEFAULT_PNG 1
+
+/* struct to hold image segment data */
+typedef struct image_segment
+{
+    int sequence_num;    /* sequence number of the image segment */
+    unsigned char *data; /* pointer to data of the image segment */
+    size_t size;         /* size of the image segment */
+} image_segment_t;
+
+/* thread input parameters struct */
+struct thread_args
+{
+    char url[MAX_URL_LENGTH];        /* url to fetch image segments */
+    int image_num;                   /* image segment number */
+    int *total_pngs_read;            /* pointer to total number of image segments read */
+    image_segment_t *image_segments; /* pointer to array of image segments */
+    pthread_mutex_t *mutex;          /* pointer to mutex for synchronizing access to shared data */
+};
+
+/* thread return values struct */
+struct thread_ret
+{
+    int done_status; /* status of thread */
+};
+
+int catpng(int num_segments, image_segment_t *segments);
+int load_png_chunks(simple_PNG_p out, image_segment_t *segment);
+void load_png_data_IHDR(struct data_IHDR *data_ihdr, U8 *segment_data, size_t sig_size, int seek_set);
